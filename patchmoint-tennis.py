01721b2ab1342c57a47c99e3191a1e5b090801f6
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2026-02-13 11:22:44 +0400

    Update patchmoint-tennis.py

--- patchmoint-tennis.py
+++ patchmoint-tennis.py
@@ -86,7 +86,7 @@ html, body, [class*="st-"], .stApp, h1, h2, h3, h4, h5, h6 {
 .dot-w { background-color: #00ff88; box-shadow: 0 0 5px #00ff88; }
 .dot-l { background-color: #ff4b4b; }
 .stApp {
-  background: linear-gradient(to bottom, #0b0c1f, #01011c);
+  background: linear-gradient(to bottom, #0b0c1f, #01010f);
 }
 @media print {
   html, body { -webkit-print-color-adjust: exact !important; print-color-adjust: exact !important; }
