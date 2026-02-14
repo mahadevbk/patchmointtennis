26fba9bea618f57300536ad01ba2222490d31052
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2026-02-14 20:11:22 +0400

    Update patchmoint-tennis.py

--- patchmoint-tennis.py
+++ patchmoint-tennis.py
@@ -165,7 +165,9 @@ h3 { font-size: 16px !important; }
 .block-container { display: flex; flex-wrap: wrap; justify-content: center; }
 [data-testid="stHorizontalBlock"] { flex: 1 1 100% !important; margin: 10px 0; }
 .chapter-card {
-    background: #222222; /* Solid dark gray background */
+    background-image: url("https://raw.githubusercontent.com/mahadevbk/patchmointtennis/main/assets/background/cardbg.png") !important;
+    background-size: cover;
+    background-position: center;
     border: 2px solid #fff500;
     border-radius: 12px;
     text-align: center;
